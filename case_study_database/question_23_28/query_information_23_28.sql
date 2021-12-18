9c5ed4eb548868af2d263f52111cd52e8c3ff35d
C0921G1-quockhanh <ledinhquockhanhdn97@gmail.com>
2021-12-18 18:07:26 +0700

    upbai

--- case_study_database/question_23_28/query_information_23_28.sql
+++ case_study_database/question_23_28/query_information_23_28.sql
@@ -84,53 +84,129 @@ DELIMITER ;
 /* 25.	Tạo Trigger có tên tr_xoa_hop_dong khi xóa bản ghi trong bảng hop_dong thì hiển thị tổng số lượng bản ghi còn lại
 có trong bảng hop_dong ra giao diện console của database.
 Lưu ý: Đối với MySQL thì sử dụng SIGNAL hoặc ghi log thay cho việc ghi ở console. */
-drop trigger if exists after_contract_delete;
-
 create table display_result (
-	count_records int,
-    old_contract_id int
+	old_contract_id int,
+	remaining_contracts int
 );
 
+drop trigger if exists after_contract_delete;
 DELIMITER //
+
 create trigger after_contract_delete after delete
 on contract
 for each row
+
 begin
-	-- declare contract_id_ int;
-	declare count_records int;
+    declare count_remaining_contracts int;
     
     select count(contract_id)
-    into count_records
+    into count_remaining_contracts
     from contract;
     
     insert into display_result
     values
-    (count_records, old.contract_id);
+    (old.contract_id, count_remaining_contracts);
 end //
+
 DELIMITER ;
 
 delete from contract
-where contract_id = 12;
+where contract_id = 1;
+
+delete from contract
+where contract_id = 2;
 
 select *
 from display_result;
 
+/* 26.	Tạo Trigger có tên tr_cap_nhat_hop_dong khi cập nhật ngày kết thúc hợp đồng, cần kiểm tra xem thời gian cập nhật có phù
+hợp hay không, với quy tắc sau: Ngày kết thúc hợp đồng phải lớn hơn ngày làm hợp đồng ít nhất là 2 ngày. Nếu dữ liệu hợp lệ thì cho phép
+cập nhật, nếu dữ liệu không hợp lệ thì in ra thông báo “Ngày kết thúc hợp đồng phải lớn hơn ngày làm hợp đồng ít nhất là 2 ngày” trên console của database. */
+
+drop trigger if exists before_contract_update;
+DELIMITER //
+
+create trigger before_contract_update before update
+on contract
+for each row
+begin
+	declare smaller_than_two boolean default false;
+    declare `check` CONDITION FOR SQLSTATE '45000';
+    
+    select abs(datediff(start_date, new.end_date)) < 2
+    into smaller_than_two
+    from contract
+    where contract_id = old.contract_id;
+    
+    if smaller_than_two is true then
+		signal `check`
+        set MESSAGE_TEXT = 'The end_date should be more than two days compared to the start_date!';
+	end if;
+end //
+
+DELIMITER ;
+
+select *
+from contract;
+
+update contract
+set end_date = '2021-01-15'
+where contract_id = 4;
+
 /* 27.	Tạo Function thực hiện yêu cầu sau
 a.	Tạo Function func_dem_dich_vu: Đếm các dịch vụ đã được sử dụng với tổng tiền là > 2.000.000 VNĐ. */
-select Ser.service_id, sum(rental_cost)
-from service Ser
-join contract Contr on Ser.service_id = Contr.service_id
-group by Contr.service_id;
 
+drop function if exists count_suitable_service;
+DELIMITER //
 
+create function count_suitable_service ()
+returns int
+deterministic
+begin 
+	declare number_of_service int;
+    
+    select count(service_id)
+    into number_of_service
+    from (
+		select Ser.service_id, sum(rental_cost) as total_rental_cost
+		from service Ser
+		join contract Contr on Ser.service_id = Contr.service_id
+		group by Contr.service_id
+		having total_rental_cost > 2000000
+    ) as tmp;
+    
+    return number_of_service;
+end //
+DELIMITER ;
+
+select count_suitable_service() as number_of_services;
 
 /* 
 b.	Tạo Function func_tinh_thoi_gian_hop_dong: Tính khoảng thời gian dài nhất tính từ lúc bắt đầu làm hợp đồng đến lúc
 kết thúc hợp đồng mà khách hàng đã thực hiện thuê dịch vụ (lưu ý chỉ xét các khoảng thời gian dựa vào từng lần làm hợp đồng
 thuê dịch vụ, không xét trên toàn bộ các lần làm hợp đồng). Mã của khách hàng được truyền vào như là 1 tham số của function này. */
 
+drop function if exists calculate_longest_time_contract;
+DELIMITER //
 
+create function calculate_longest_time_contract(id int)
+returns int
+deterministic
+begin
+	declare max_time_by_customer int;
+    
+	select max_time
+    into max_time_by_customer
+    from (
+		select customer_id, max(abs(datediff(start_date, end_date))) as max_time
+		from contract
+		group by customer_id
+    ) as tmp
+    where tmp.customer_id = id;
+    
+    return max_time_by_customer;
+end //
 
-/* 28.	Tạo Stored Procedure sp_xoa_dich_vu_va_hd_room để tìm các dịch vụ được thuê bởi khách hàng với loại dịch vụ là “Room”
-từ đầu năm 2015 đến hết năm 2019 để xóa thông tin của các dịch vụ đó (tức là xóa các bảng ghi trong bảng dich_vu) và xóa những
-hop_dong sử dụng dịch vụ liên quan (tức là phải xóa những bản gi trong bảng hop_dong) và những bản liên quan khác. */
+DELIMITER ;
+
+select calculate_longest_time_contract(1);
