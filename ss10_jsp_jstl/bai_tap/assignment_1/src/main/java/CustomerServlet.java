c661ba221946942f8fff6fa1658af2ccd1f763a0
C0921G1-quockhanh <ledinhquockhanhdn97@gmail.com>
2021-12-25 17:51:11 +0700

    upbai

---
+++ ss10_jsp_jstl/bai_tap/assignment_1/src/main/java/CustomerServlet.java
@@ -0,0 +1,28 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+@WebServlet(name = "CustomerServlet", urlPatterns = {"/", "/customer"})
+public class CustomerServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        List<Customer> customers = new ArrayList<>();
+
+        customers.add(new Customer("Ronaldo","05-02-1985","Portugal","/resources/img/ronaldo.jpg"));
+        customers.add(new Customer("Messi","24-06-1987","Argentina","/resources/img/messi.jpg"));
+        customers.add(new Customer("Neymar","05-02-1992","Brazil","/resources/img/neymar.jpg"));
+        customers.add(new Customer("Haaland","21-07-2000","Norway","/resources/img/haaland.jpg"));
+        
+        request.setAttribute("customers",customers);
+        request.getRequestDispatcher("customer_list.jsp")
+                .forward(request,response);
+    }
+}
---
+++ ss10_jsp_jstl/bai_tap/assignment_1/src/main/webapp/customer_list.jsp
@@ -0,0 +1,44 @@
+<%@ page import="java.util.List" %><%--
+  Created by IntelliJ IDEA.
+  User: AD
+  Date: 12/25/2021
+  Time: 3:48 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib uri = "http://java.sun.com/jsp/jstl/core" prefix = "c" %>
+<html>
+<head>
+    <title>Customer_List</title>
+</head>
+<body>
+    <h2>Customer List</h2>
+    <table border="1">
+        <tr>
+            <th>Name</th>
+            <th>Date of Birth</th>
+            <th>Address</th>
+            <th>Image</th>
+        </tr>
+
+        <c:forEach var="customer" items="${customers}">
+            <tr>
+                <td>
+                    <c:out value="${customer.name}"/>
+                </td>
+                <td>
+                    <c:out value="${customer.dateOfBirth}"/>
+                </td>
+                <td>
+                    <c:out value="${customer.address}"/>
+                </td>
+                <td>
+                    <img src="<c:url value="${customer.image}"/>" alt="footballer_image">
+                </td>
+            </tr>
+        </c:forEach>
+    </table>
+
+    <img src="resources/img/ronaldo.jpg" alt="picture">
+</body>
+</html>
