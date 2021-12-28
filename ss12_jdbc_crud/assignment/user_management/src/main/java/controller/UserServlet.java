ebb24e2ecc6e74bace61e0b34ecd71bdc264b0e3
C0921G1-quockhanh <ledinhquockhanhdn97@gmail.com>
2021-12-28 23:36:57 +0700

    upbai

---
+++ ss12_jdbc_crud/assignment/user_management/src/main/java/controller/UserServlet.java
@@ -0,0 +1,151 @@
+package controller;
+
+import bean.User;
+import service.IUserService;
+import service.UserService;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
+
+@WebServlet(name = "UserServlet",urlPatterns = "/users")
+public class UserServlet extends HttpServlet {
+
+    private static final long serialVersionUID = 1L;
+    private IUserService iUserService = new UserService();
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String action = request.getParameter("action");
+
+        if (action == null)
+            action = "";
+
+        try {
+            switch (action) {
+                case "create":
+                    insertUser(request,response);
+                    break;
+
+                case "edit":
+                    updateUser(request,response);
+                    break;
+
+                case "find_by_country":
+                    findUserByCountry(request,response);
+                    break;
+            }
+        }
+        catch (SQLException ex) {
+            throw new ServletException(ex);
+        }
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String action = request.getParameter("action");
+
+        if (action == null)
+            action = "";
+
+        try {
+            switch (action) {
+                case "create":
+                    showNewForm(request,response);
+                    break;
+
+                case "edit":
+                    showEditForm(request,response);
+                    break;
+
+                case "delete":
+                    deleteUser(request,response);
+                    break;
+
+                case "find_by_country":
+                    showFindUserForm(request,response);
+                    break;
+
+                default:
+                    listUser(request,response);
+            }
+        }
+        catch (SQLException ex) {
+            throw new ServletException(ex);
+        }
+    }
+
+    private void showFindUserForm(HttpServletRequest request, HttpServletResponse response) throws SQLException, IOException, ServletException {
+        RequestDispatcher dispatcher = request.getRequestDispatcher("user/find_by_country.jsp");
+        dispatcher.forward(request,response);
+    }
+
+    private void findUserByCountry(HttpServletRequest request,HttpServletResponse response) throws SQLException, IOException, ServletException {
+        String existingCountry = request.getParameter("country");
+        List<User> users = this.iUserService.findUserByCountry(existingCountry);
+
+        request.setAttribute("users",users);
+        RequestDispatcher dispatcher = request.getRequestDispatcher("user/find_by_country.jsp");
+        dispatcher.forward(request,response);
+    }
+
+    private void listUser(HttpServletRequest request,HttpServletResponse response) throws SQLException, IOException, ServletException {
+        List<User> users = this.iUserService.selectAllUsers();
+        request.setAttribute("users",users);
+        RequestDispatcher dispatcher = request.getRequestDispatcher("user/list.jsp");
+        dispatcher.forward(request,response);
+    }
+
+    private void showNewForm(HttpServletRequest request,HttpServletResponse response) throws SQLException, IOException, ServletException {
+        RequestDispatcher dispatcher = request.getRequestDispatcher("user/create.jsp");
+        dispatcher.forward(request,response);
+    }
+
+    private void insertUser(HttpServletRequest request,HttpServletResponse response) throws SQLException, IOException, ServletException {
+        String name = request.getParameter("name");
+        String email = request.getParameter("email");
+        String country = request.getParameter("country");
+
+        User newUser = new User(name,email,country);
+        this.iUserService.insertUser(newUser);
+
+        RequestDispatcher dispatcher = request.getRequestDispatcher("user/create.jsp");
+        dispatcher.forward(request,response);
+    }
+
+    private void showEditForm(HttpServletRequest request,HttpServletResponse response) throws SQLException, IOException, ServletException {
+        int id = Integer.parseInt(request.getParameter("id"));
+        User existingUser = this.iUserService.selectUser(id);
+
+        RequestDispatcher dispatcher = request.getRequestDispatcher("user/edit.jsp");
+        request.setAttribute("existingUser",existingUser);
+        dispatcher.forward(request,response);
+    }
+
+    private void updateUser(HttpServletRequest request,HttpServletResponse response) throws SQLException, IOException, ServletException {
+        int id = Integer.parseInt(request.getParameter("id"));
+        String name = request.getParameter("name");
+        String email = request.getParameter("email");
+        String country = request.getParameter("country");
+
+        User updatedUser = new User(id,name,email,country);
+        this.iUserService.updateUser(updatedUser);
+
+        RequestDispatcher dispatcher = request.getRequestDispatcher("user/edit.jsp");
+        dispatcher.forward(request,response);
+    }
+
+    private void deleteUser(HttpServletRequest request,HttpServletResponse response) throws SQLException, IOException, ServletException {
+        int id = Integer.parseInt(request.getParameter("id"));
+        this.iUserService.deleteUser(id);
+
+        List<User> users = this.iUserService.selectAllUsers();
+        request.setAttribute("users",users);
+        RequestDispatcher dispatcher = request.getRequestDispatcher("user/list.jsp");
+        dispatcher.forward(request,response);
+    }
+}
---
+++ ss12_jdbc_crud/assignment/user_management/src/main/webapp/user/find_by_country.jsp
@@ -0,0 +1,72 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: AD
+  Date: 12/28/2021
+  Time: 11:17 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<html>
+<head>
+    <title>User Management Application</title>
+</head>
+<body>
+    <div align="center">
+        <h1>User Management</h1>
+        <h2>
+            <a href="/users">Back to user list!</a>
+        </h2>
+    </div>
+
+    <div align="center">
+        <form method="post">
+            <table border="1" cellpadding="5">
+                <caption>
+                    <h2>Find user: </h2>
+                </caption>
+
+                <tr>
+                    <td>User country: </td>
+                    <td>
+                        <input type="text" name="country" size="45">
+                    </td>
+                    <td>
+                        <input type="submit" value="Find user">
+                    </td>
+                </tr>
+            </table>
+        </form>
+    </div>
+
+    <c:if test="${users != null}">
+        <div align="center">
+            <table border="1" cellpadding="5">
+                <tr>
+                    <th>ID</th>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Country</th>
+                </tr>
+
+                <c:forEach var="user" items="${users}">
+                    <tr>
+                        <td>
+                            <c:out value="${user.id}"/>
+                        </td>
+                        <td>
+                            <c:out value="${user.name}"/>
+                        </td>
+                        <td>
+                            <c:out value="${user.email}"/>
+                        </td>
+                        <td>
+                            <c:out value="${user.country}"/>
+                        </td>
+                    </tr>
+                </c:forEach>
+            </table>
+        </div>
+    </c:if>
+</body>
+</html>
